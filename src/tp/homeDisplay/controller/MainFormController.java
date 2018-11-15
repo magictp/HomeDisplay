75fb81f176db61688bf8ce3e4a8151bfe49e43d8
magictp <8610724+magictp@users.noreply.github.com>
2018-11-15 15:10:49 +0900

    Update MainFormController.java

--- src/tp/homeDisplay/controller/MainFormController.java
+++ src/tp/homeDisplay/controller/MainFormController.java
@@ -1,10 +1,25 @@
 package tp.homeDisplay.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import tp.homeDisplay.model.Weather;
+import tp.homeDisplay.model.YahooQuery;
 import tp.homeDisplay.service.MetroService;
+import tp.homeDisplay.service.WeatherService;
+
 
 public class MainFormController {
 
 	public String getOncomingMetro(){
 		return new MetroService().getOncomingShift();
+	}
+	
+	public String getWeatherForecast() throws Exception{
+		String jsonString = new WeatherService().getWeatherForecast();
+		jsonString = jsonString.substring(9, jsonString.length() -1);
+		YahooQuery<Weather> yw = (YahooQuery<Weather>)new Gson().fromJson(jsonString,new TypeToken<YahooQuery<Weather>>(){}.getType());
+		Weather w = (Weather)yw.getResults();
+		return w.getTemp();
 	}
 }
