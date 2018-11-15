3d10d2b98bb2f9c6b24f5f107744df47bc165e4d
magictp <8610724+magictp@users.noreply.github.com>
2018-11-15 17:43:52 +0900

    Update MainFormController.java

--- src/tp/homeDisplay/controller/MainFormController.java
+++ src/tp/homeDisplay/controller/MainFormController.java
@@ -3,9 +3,12 @@ package tp.homeDisplay.controller;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import tp.homeDisplay.model.Article;
+import tp.homeDisplay.model.NewsApiQuery;
 import tp.homeDisplay.model.Weather;
 import tp.homeDisplay.model.YahooQuery;
 import tp.homeDisplay.service.MetroService;
+import tp.homeDisplay.service.NewsService;
 import tp.homeDisplay.service.WeatherService;
 
 
@@ -21,5 +24,18 @@ public class MainFormController {
 		YahooQuery<Weather> yw = (YahooQuery<Weather>)new Gson().fromJson(jsonString,new TypeToken<YahooQuery<Weather>>(){}.getType());
 		Weather w = (Weather)yw.getResults();
 		return w.getTemp();
+	}
+	
+	public String getLatestNews() throws Exception{
+		String newsString = "";
+		String jsonString = new NewsService().getLatestNews();
+		NewsApiQuery news = new Gson().fromJson(jsonString,new TypeToken<NewsApiQuery>(){}.getType());
+		Article[] arts = news.getArticles();
+		for(Article a : arts)
+		{
+			newsString += a.getTitle();
+			newsString += "          ";
+		}
+		return newsString;
 	}
 }
