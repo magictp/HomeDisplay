421cbda2066847898f1867a8b705e5700ccd9aa0
tp <tp.ecust@gmail.com>
2019-10-22 10:53:54 +0900

    add clock and metro info

---
+++ build.sh
@@ -0,0 +1,1 @@
+javac -cp "/home/pi/apache-tomcat-9.0.0.M21/lib/*:WebContent/WEB-INF/lib/*" @source.txt
--- src/tp/homeDisplay/controller/MainFormController.java
+++ src/tp/homeDisplay/controller/MainFormController.java
@@ -3,21 +3,32 @@ package tp.homeDisplay.controller;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import tp.homeDisplay.model.Article;
-import tp.homeDisplay.model.NewsApiQuery;
-import tp.homeDisplay.model.Weather;
-import tp.homeDisplay.model.YahooQuery;
+//import tp.homeDisplay.model.Article;
+//import tp.homeDisplay.model.NewsApiQuery;
+//import tp.homeDisplay.model.Weather;
+//import tp.homeDisplay.model.YahooQuery;
 import tp.homeDisplay.service.MetroService;
-import tp.homeDisplay.service.NewsService;
-import tp.homeDisplay.service.WeatherService;
+//import tp.homeDisplay.service.NewsService;
+//import tp.homeDisplay.service.WeatherService;
+import com.opensymphony.xwork2.ActionSupport;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+public class MainFormController extends ActionSupport{
 
-
-public class MainFormController {
-
-	public String getOncomingMetro(){
-		return new MetroService().getOncomingShift();
+        private InputStream inputStream;
+	public InputStream getInputStream() {
+		return inputStream;
 	}
-	
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+	public String getOncomingMetro(){
+		String nextTime =  new MetroService().getOncomingShift();
+		inputStream = new ByteArrayInputStream(nextTime.getBytes(StandardCharsets.UTF_8));
+		return SUCCESS;	
+	}
+/*	
 	public String getWeatherForecast() throws Exception{
 		String jsonString = new WeatherService().getWeatherForecast();
 		jsonString = jsonString.substring(9, jsonString.length() -1);
@@ -37,5 +48,5 @@ public class MainFormController {
 			newsString += "          ";
 		}
 		return newsString;
-	}
+	}*/
 }
